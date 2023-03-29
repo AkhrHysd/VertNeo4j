0385d2e31244fb2cf4b52aa0f068b6afe8b6e1b0
AkhrHysd <yaurevoci@gmail.com>
2023-03-30 01:06:52 +0900

    fix

--- run_migration.sh
+++ run_migration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # マイグレーションファイルのパスを変数に設定します
-MIGRATION_FILE_PATH="./migrations/migration-file.cypher"
+MIGRATION_FILE_PATH="./migration/migration-file.cypher"
 
 # マイグレーションファイルをコンテナにコピーします
 docker cp $MIGRATION_FILE_PATH neo4j_container:/var/lib/neo4j/import/
