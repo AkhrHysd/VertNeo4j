825fe75626d2321fd5a19f8a6f5029394d9fcfbc
AkhrHysd <yaurevoci@gmail.com>
2023-03-30 01:10:00 +0900

    fix container name

--- run_migration.sh
+++ run_migration.sh
@@ -4,7 +4,7 @@
 MIGRATION_FILE_PATH="./migration/migration-file.cypher"
 
 # マイグレーションファイルをコンテナにコピーします
-docker cp $MIGRATION_FILE_PATH neo4j_container:/var/lib/neo4j/import/
+docker cp $MIGRATION_FILE_PATH vertneo4j-neo4j-1:/var/lib/neo4j/import/
 
 # コンテナ内でマイグレーションを実行します
-docker exec neo4j_container neo4j-admin load --from=/var/lib/neo4j/import/migration-file.cypher --database=neo4j
+docker exec vertneo4j-neo4j-1 neo4j-admin load --from=/var/lib/neo4j/import/migration-file.cypher --database=neo4j
